6a9a4adb0d45cdc53b11af3bb4bd38831e3c6ade
guilt98 <124409817+guilt98@users.noreply.github.com>
2023-04-11 21:34:11 -0300

    Add files via upload

---
+++ questoes java.js
@@ -0,0 +1,62 @@
+var char = ('m');
+if (dados=char);{
+    console.log("É um valor char");
+}
+
+var int = ('21');
+if(int=('int'));{
+    console.log("é um valor inteiro");
+}
+
+
+var float= ('3.1415');
+if (float=('float'));{
+    console.log('é um valor real');
+}
+
+var string = ("O resultado é");
+if (string=('string'));{
+    console.log('é Um valor string');
+}
+
+var boolean = ('true');
+if (boolean=('boolean'));{
+    console.log('Valor boolean');
+}
+
+var string2 = ("459");
+if (string2=('string'));{
+    console.log('Valor String');
+}
+
+var char2 = ('h');
+if (char2=('char'));{
+    console.log('É um valor char');
+}
+
+var int2 = 12;
+var int3 = 6;
+var reais = -3;
+var Float = 1.5;
+var s2=(2*int2)%3-reais;{
+    console.log(s2);
+}
+
+var s3=(-2*reais)/4;{
+    console.log(s3);
+}
+
+var s4= Math.pow((20%3),3)+Math.pow(2,8)/2;{
+    console.log(s4);
+}
+    
+var s5= Math.sqrt(30%4* Math.pow(3,3))*-1;{
+    console.log(s5);
+}
+
+var s6= Math.pow(-reais,2)+(Float*10)/int2;{
+    console.log(s6);
+}
+
+var s7=
+
